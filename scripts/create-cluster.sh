98dbd6af28ff52d065fad54fb08c0cc28174c383
larntz <larntz@dell-r710.dhcp.chacarntz.net>
2021-10-27 05:57:04 -0400

    modify create script and artifact dir

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 if [ -d $PWD/packer-builds ]; then 
   echo $'\n\nalready packed, proceeding to terraform\n'
-  echo ""
+  packer build packer/debian.json
 else
   echo $'\n\npack it up, pack it in, let us begin'
-  echo $'packing'
 fi
 
 # create vms and ansible host inventory
-echo "terraform -chdir=./terraform/ apply -auto-approve"
+terraform -chdir=./terraform/ init
+terraform -chdir=./terraform/ apply -auto-approve
 
 # run asible playbook to configure cluster lb and kubespray global vars
-echo "ansible-playbook -i cluster-hosts ansible-playbooks/configure-cluster.yaml"
+ansible-playbook -i cluster-hosts ansible-playbooks/configure-cluster.yaml
 
 echo "finished."
-echo "export KUBECONFIG=$PWD/artifacts/admin.conf to access cluster"
 
+if [ $? == 0 ]; then 
+  echo "export KUBECONFIG=$PWD/artifacts/admin.conf to access cluster."
+else 
+  echo "something went wrong."
+fi
